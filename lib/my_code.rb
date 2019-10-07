f0c93eb1668d05c1ec39e65d376efecbf5ae5c0f
Allison Johnson <allison.cuttler@gmail.com>
2019-10-07 16:19:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,10 +8,13 @@ def map(source_array)
   return new_array
 end
 
-#Negativize
+def reduce(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array << yield(source_array[i])
+    i += 1
+  end #end while
+  return new_array
+end
 
-#No change
-
-#Double
-
-#Square
